b856618a7c6504d92094d4c6aba60b6fd8454674
wangliangliang <wangliangliang@vipkid.com.cn>
2019-06-02 14:22:54 +0800

    添加demo-first-boot工程

---
+++ demo-first-boot/src/main/java/com/test/demofirstboot/DemoFirstBootApplication.java
@@ -0,0 +1,13 @@
+package com.test.demofirstboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFirstBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFirstBootApplication.class, args);
+	}
+
+}
